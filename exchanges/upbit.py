f32aa40993e9e059fecf87bced069d986c707448
이형진 <ihyeongjin@ihyeongjin-ui-MacBookPro.local>
2024-09-03 14:52:10 +0900

    feat: skeleton of sdk

---
+++ exchanges/upbit.py
@@ -0,0 +1,32 @@
+from decimal import Decimal
+
+from exchange import Exchange, Order, OrderBook
+
+
+class Upbit(Exchange):
+    def __init__(self, api_key: str, secret: str):
+        super().__init__(
+            api_key=api_key,
+            secret=secret
+        )
+
+    def buy(self, symbol: str, price: Decimal, amount: Decimal) -> Order:
+        pass
+
+    def sell(self, symbol: str, price: Decimal, amount: Decimal) -> Order:
+        pass
+
+    def withdraw_to(self, symbol: str, price: Decimal, amount: Decimal) -> bool:
+        pass
+
+    def make_sign(self) -> str:
+        pass
+
+    def order_fetch(self, ord_id: str, symbol: str) -> Order:
+        pass
+
+    def get_orderbook(self, symbol) -> OrderBook:
+        pass
+
+    def get_coin_list(self) -> list[str]:
+        pass
